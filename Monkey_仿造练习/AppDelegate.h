652526c553405e1bb60168ee9251b184cc2db2be
jksdzh <kangshuaijin@kangshuais-macbook-pro.local>
2016-08-25 16:26:38 +0800

    Initial Commit

---
+++ Monkey_仿造练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Monkey_仿造练习
+//
+//  Created by 康帅 金 on 2016/8/25.
+//  Copyright © 2016年 康帅 金. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
